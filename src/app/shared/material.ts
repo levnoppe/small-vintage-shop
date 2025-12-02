2443447d9bcf761eb567cb1058d31daf2a7bf230
levnoppe <noppe.lev@gmail.com>
2025-12-02 07:50:31 +0000

    add angular material

---
+++ src/app/shared/material.ts
@@ -0,0 +1,11 @@
+export const MATERIAL_IMPORTS = [
+  // Angular Material
+  import('@angular/material/button').then(m => m.MatButtonModule),
+  import('@angular/material/icon').then(m => m.MatIconModule),
+  import('@angular/material/card').then(m => m.MatCardModule),
+  import('@angular/material/toolbar').then(m => m.MatToolbarModule),
+  import('@angular/material/input').then(m => m.MatInputModule),
+  import('@angular/material/menu').then(m => m.MatMenuModule),
+  import('@angular/material/sidenav').then(m => m.MatSidenavModule),
+  import('@angular/material/list').then(m => m.MatListModule)
+];
